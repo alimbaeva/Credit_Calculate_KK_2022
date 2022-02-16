9aa8c40747b2ce76cc47ffde8f26c78fc0164971
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-16 12:20:38 +0300

    feat: added function recalculation, btn.addEventListener

---
+++ js.js
@@ -0,0 +1,32 @@
+const amount = document.querySelector('#amount');
+const interest = document.querySelector('#interest');
+const years = document.querySelector('#years');
+const btn = document.querySelector('.btn');
+
+const monthlyPayment = document.querySelector('#monthly-paymentIN');
+const total = document.querySelector('#totalIN');
+const totalInterest = document.querySelector('#total-interestIN');
+
+// console.log(monthlyPayment.value = '111111')
+// console.log(total)
+// console.log(totalInterest)
+
+function recalculation(amountValue, interestValue, yearsValue) {
+    const totalInterestValue = ((amountValue * interestValue / 100) * yearsValue).toFixed(2);
+    const totalValue = Number(totalInterestValue) + Number(amountValue);
+    const monthlyPaymentValue = (totalValue % (yearsValue * 12)).toFixed(2);
+    monthlyPayment.value = monthlyPaymentValue;
+    total.value = totalValue;
+    totalInterest.value = totalInterestValue;
+
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const amountValue = amount.value;
+    const interestValue = interest.value;
+    const yearsValue = years.value;
+    recalculation(amountValue, interestValue, yearsValue);
+
+})
+
